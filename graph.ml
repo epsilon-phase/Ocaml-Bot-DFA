de4f7230d3b38b8915500aeabc17314523ea4872
Violet white <violet.white.dammit@protonmail.com>
2019-11-21 11:50:47 -0500

    Improved graph generation further :)

--- graph.ml
+++ graph.ml
@@ -24,7 +24,7 @@ let rec convert_to_graph (counter : unit -> int) (t : terminal) =
   | LocalAssign (s, b) -> LocalAssign (s, convert_to_graph counter b, i)
   | Symbol s -> Symbol (s, i)
   | Dictionary l -> Dictionary (List.map (convert_to_graph counter) l, i)
-  | Leaf s -> Leaf (s, i)
+  | Leaf s -> Leaf (Str.global_replace (Str.regexp "\\") "\\0\\0" s, i)
   | Null -> Null i
   | Sequence l -> Sequence (List.map (convert_to_graph counter) l, i)
 
@@ -34,8 +34,15 @@ let rec write_node output (t : graph_terminal) =
   match t with
   | Leaf (s, i) -> output_string output @@ make_node s i
   | Null i -> output_string output @@ make_node "∅" i
-  | Dictionary (l, _) -> List.iter (write_node output) l
+  | Dictionary (l, i) ->
+      output_string output @@ make_node "Dict{" i ;
+      List.iter (write_node output) l
   | Sequence (l, _) -> List.iter (write_node output) l
+  | LocalAssign (s, n, i) ->
+      output_string output @@ make_node (String.concat "" [s; "<-"]) i ;
+      write_node output n
+  | LocalReference (s, i) ->
+      output_string output @@ make_node (String.concat "" [s; "->"]) i
   | _ -> ()
 
 let rec obtain_ids (tbl : (terminal, int) Hashtbl.t) (t : terminal)
@@ -62,7 +69,7 @@ let id_of (t : graph_terminal) =
 let rec obtain_edges (edges : (int, int) Hashtbl.t)
     (previous : int list option) (t : graph_terminal) : int list option =
   let this_id = id_of t in
-  let add_id = match t with Leaf _ -> true | Null _ -> true | _ -> false in
+  let add_id = match t with Sequence _ -> false | _ -> true in
   ( match previous with
   | Some x ->
       if add_id then List.iter (fun i -> Hashtbl.add edges i this_id) x
@@ -74,17 +81,13 @@ let rec obtain_edges (edges : (int, int) Hashtbl.t)
         l
         (match previous with Some x -> Some x | None -> Some [this_id])
   | Dictionary (l, _) ->
-      let associations =
-        List.map
-          (obtain_edges edges
-             (match previous with None -> Some [this_id] | Some x -> Some x))
-          l
-      in
+      let associations = List.map (obtain_edges edges (Some [this_id])) l in
       Some
         ( List.concat
         @@ List.map
              (fun x -> match x with Some x -> x | _ -> [])
              associations )
+  | LocalAssign (_, n, _) -> obtain_edges edges (Some [this_id]) n
   | x -> Some [id_of x]
 
 let () =
